8757ff541c65843c760afcd285a55021451c7b79
sudarshan411 <sudarshan26411@gmail.com>
2021-10-12 00:40:51 +0530

    Added user model

---
+++ models/users.model.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs")
+
+const Schema = mongoose.Schema;
+
+const UserSchema = Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    }
+});
+
+UserSchema.pre("save", function(next){
+    const user = this
+
+    if(this.isModified("password") || this.isNew){
+        bcrypt.genSalt(10, function (saltError, salt){
+            if(saltError){
+                return next(saltError)
+            } else {
+                bcrypt.hash(user.password, salt, function(hashError, hash){
+                    if (hashError){
+                        return next(hashError)
+                    }
+
+                    user.password = hash
+                    next()
+                })
+            }
+        })
+    } else {
+        return next()
+    }
+})
+
+module.exports = mongoose.model("User", UserSchema)
